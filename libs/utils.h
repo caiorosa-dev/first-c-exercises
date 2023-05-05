4de36a591523aa216d7eb91ee4e38b91cf724861
Bugado <addminplayerbr1122@gmail.com>
2023-05-05 16:19:26 -0300

    feat: New functions related to text

--- libs/utils.h
+++ libs/utils.h
@@ -26,6 +26,41 @@ int readFloat() {
     return value;
 }
 
+char* centralizeString(int widthToCentralize, const char *stringToCentralize) {
+    int stringSize = (int) strlen(stringToCentralize);
+    int spaces = (widthToCentralize - stringSize) / 2;
+
+    char* finalString = malloc(widthToCentralize * 2);
+    sprintf(finalString, "");
+
+    for (int i = 0; i < spaces; ++i) {
+        strcat(finalString, " ");
+    }
+
+    strcat(finalString, stringToCentralize);
+
+    for (int i = 0; i < spaces; ++i) {
+        strcat(finalString, " ");
+    }
+
+    return finalString;
+}
+
+char* centralizeStringList(int widthToCentralize, const char stringList[][10], const int lines) {
+    char* finalString = malloc(widthToCentralize * 2);
+    sprintf(finalString, "");
+
+    for (int i = 0; i < lines; ++i) {
+        char* line = stringList[i];
+        char* centralizedString = centralizeString(widthToCentralize, line);
+
+        strcat(finalString, centralizedString);
+        if ((i + 1) < lines) strcat(finalString, "\n");
+    }
+
+    return finalString;
+}
+
 void clearTerminal() {
     system("cls");
 }
